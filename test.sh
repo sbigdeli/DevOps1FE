e2c9842dcea16537f376ce03e7415acf03e086a7
Vedmastaren <117941745+Vedmastaren@users.noreply.github.com>
2024-05-29 09:15:08 +0200

    testing with a test file

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Read current version from version.txt
+VERSION=$(<version.txt)
+
+# Bump version number (example: increment by 1)
+NEW_VERSION=$((VERSION + 1))
+
+# Write new version back to version.txt
+echo "${NEW_VERSION}" > version.txt
+
+# Optionally, you can do more actions here based on your versioning needs
+# For example, update version in package.json or other files
+# sed -i "s/\"version\": \".*\"/\"version\": \"${NEW_VERSION}\"/" package.json
