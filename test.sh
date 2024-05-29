9949931890aba2d668f4d4bcadb5111261c3a561
Vedmastaren <117941745+Vedmastaren@users.noreply.github.com>
2024-05-29 09:21:09 +0200

    testing again

--- test.sh
+++ test.sh
@@ -9,6 +9,6 @@ NEW_VERSION=$((VERSION + 1))
 # Write new version back to version.txt
 echo "${NEW_VERSION}" > version.txt
 
-# Optionally, you can do more actions here based on your versioning needs
+# Optionally, you can do more actions here based on your versioning needss
 # For example, update version in package.json or other files
 # sed -i "s/\"version\": \".*\"/\"version\": \"${NEW_VERSION}\"/" package.json
