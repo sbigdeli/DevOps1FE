10834a2bd1365bb93b2591b067fec93449a1f353
lukas <lukas.engdahl@hotmail.com>
2024-05-29 11:07:31 +0200

    removed unnescessary files.

--- dev.sh
+++ dev.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Read current version from version.txt
-VERSION=$(<version.txt)
+VERSION=$(<version.md)
 
 # Bump version number (example: increment by 1)
 NEW_VERSION=$((VERSION + 1))
 
 # Write new version back to version.txt
-echo "${NEW_VERSION}" > version.txt
+echo "${NEW_VERSION}" > version.md
 
 # Optionally, you can do more actions here based on your versioning needsssss
 # For example, update version in package.json or other files
